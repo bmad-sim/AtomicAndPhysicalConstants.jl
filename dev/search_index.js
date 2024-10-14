f6e89a14e0a53c3a46cd7c0b05d9dfdd48659cb8
Documenter.jl <documenter@juliadocs.github.io>
2024-10-14 16:59:45 +0000

    build based on 24f0cee

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#AtomicAndPhysicalConstants.jl","page":"Home","title":"AtomicAndPhysicalConstants.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Hi!","category":"page"}]
+}
