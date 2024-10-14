daa4f3e5a7f0d19689419b220c9b1fae0f9534c7
Documenter.jl <documenter@juliadocs.github.io>
2024-10-14 17:10:30 +0000

    build based on 48efc5a

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#AtomicAndPhysicalConstants.jl","page":"Home","title":"AtomicAndPhysicalConstants.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Hi! Look at my equation:","category":"page"},{"location":"","page":"Home","title":"Home","text":"math langle P rangle_t = 075","category":"page"}]
+[{"location":"#AtomicAndPhysicalConstants.jl","page":"Home","title":"AtomicAndPhysicalConstants.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Hi! Look at my equation:","category":"page"},{"location":"","page":"Home","title":"Home","text":"langle P rangle_t = 075","category":"page"}]
 }
